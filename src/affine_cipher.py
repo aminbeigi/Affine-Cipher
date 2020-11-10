4773928a4560d8cdf8cd9801cc6726b5c24a8614
Amin Ghasembeigi <me@aminbeigi.com>
2020-11-10 11:45:37 +1100

    Create affine_cypher.py

---
+++ src/affine_cipher.py
@@ -0,0 +1,8 @@
+def affine_cipher(x):
+    print("test")
+
+def main():
+    print("An affine cipher is applied to encrypt a message whose (unencrypted) message is coded using A = 1, B = 2, ..., Y = 25, Z = 26")
+
+if __name__ == '__main__':
+    main()
